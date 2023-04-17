c0849f20190bf6da4ad56048804788bbf55f44fa
Damian Poręba <damianporeba98@gmail.com>
2023-04-17 20:53:23 +0200

    Add project files.

---
+++ EvenNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        //założenia
+        int begin, end;
+
+
+        Console.WriteLine("Witaj w programie do wyszukiwania licz parzystych z podanego zakresu \npodaj początek zakresu...");
+        begin = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Podaj koniec zakresu");
+        end = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Liczby parzyste podanego zakresu to:");
+
+        do
+        {
+            
+            if (begin % 2 == 0)
+            {
+                Console.WriteLine(begin);
+            }
+            begin++;
+        }
+        while (begin < end);
+        
+       
+
+    }
+}
